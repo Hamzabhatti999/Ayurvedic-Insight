9adb66c5020ece1c9a08bc0ff0a8a1f282c8944c
HamzaBhatti999 <htkbhatti157@gmail.com>
2024-10-12 14:57:43 +0500

    home page completed

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,6 @@
 import backgroundImage from "../assets/rectangle.png";
+import homeImage from "../assets/Rectangle 4.png";
+import homeImage1 from "../assets/splash.png";
 import Navbar from "../components/navbar";
 export default function Home() {
   return (
@@ -8,10 +10,27 @@ export default function Home() {
         backgroundImage: `url(${backgroundImage})`,
       }}
     >
-      <div className="container mx-auto">
-        <Navbar />
+      <Navbar />
+
+      <div className="container mx-auto mt-20">
+        <div className="flex flex-col gap-4 md:flex-row justify-between items-center">
+          <div className="md:w-2/5 text-balance px-20">
+            <h1 className="text-5xl font-extrabold text-black">
+              Discover Harmony and Healing with{" "}
+              <span className="text-lime-500">Ayurveda</span>
+            </h1>
+            <p className="text-lg text-gray-600 mt-4 font-medium">
+              Ayurveda views health and well-being as a delicate balance between
+              the body, mind, and spirit. It emphasizes the importance of
+              maintaining this balance to achieve optimal health.
+            </p>
+          </div>
+          <div className="lg:w-1/2 px-10 lg:px-0 flex flex-col lg:flex-row">
+            <img src={homeImage} alt="Ayurveda image" className="h-[400px]" />
+            <img src={homeImage1} alt="Ayurveda image" className="h-96 mt-8" />
+          </div>
+        </div>
       </div>
-      <div className="container"></div>
     </div>
   );
 }
