72b22f7755972eaff4dc68e9161a05cd4e2763c3
HamzaBhatti999 <htkbhatti157@gmail.com>
2024-10-14 16:18:40 +0500

    testimonials

--- src/pages/links.tsx
+++ src/pages/links.tsx
@@ -1,10 +1,8 @@
 const Links = () => {
   return (
     <div>
-      <h3 className="my-4 text-4xl text-center font-semibold italic">
-        Monika Joshi
-      </h3>
-      <div className="px-20 text-justify pb-10 font-semibold">
+      <h3 className="my-4 text-4xl text-center font-semibold italic">Links</h3>
+      <div className="px-20 text-justify pb-10">
         <p>
           Monika is a Clinical Ayurvedic Specialist who graduated from
           California College of Ayurveda and is also certified by American
@@ -24,7 +22,6 @@ const Links = () => {
           Ayurveda through her own personal and medical issues including losing
           family members to cancers. Ayurveda has become a passion of her life.
         </p>
-
         <p className="mb-4">
           Born and brought up in India, land of Ayurveda, it is in the “West”
           where she found her passion in the most amazing ‘science of life’. The
@@ -33,6 +30,40 @@ const Links = () => {
           the most fascinating part of Ayurveda. Journey to health begins when
           one tries to bring the harmony back in life.
         </p>
+        The Center is a unique gathering of healers and cultural creatives whose
+        mission is to facilitate integrity, healing, heart work, and conscious
+        knowing. The Center offers a holistic approach to well being with a
+        variety of healing arts professionals, including an intuitive healer, a
+        holistic physician, massage and shiatsu therapists, a marriage and
+        family therapist, an ayurvedic specialist, and a counselor, as well as
+        fine art, good music, and a unique lending library to help balance and
+        integrate body, mind, and spirit. Ayurveda College California College of
+        Ayurveda (CCA) is an internationally recognized, state-approved school
+        offering a two year, clinically oriented education program leading to
+        certification as a Clinical Ayurvedic Specialist (CAS). CCA is dedicated
+        to providing the highest quality education, health care, and research in
+        the field of Ayurveda. It trains students to become the healers of the
+        future; Ayurvedic practitioners dedicated to understanding the whole
+        person physically, emotionally, and spiritually. Yoga For Living Serving
+        the central valley of California since 2001, Yoga For Living is
+        recognized by John Friend, the founder of Anusara Yoga as an Anusara
+        Inspired Studio. With a focus on clear, intelligent instruction Yoga For
+        Living has earned a reputation as one of the premier studios in the
+        area, suitable for practitioners of all levels. Rejuvenation Therapies
+        Located in Grass Valley CA, Rejuvenation Therapies specializes in Pancha
+        Karma detoxification and stress-reduction programs. The most popular
+        stand-alone treatment is the Bliss therapy, which includes a two-person
+        synchronized oil massage, shirodhara and herbal steam therapy. It is the
+        basis of the pancha karma programs, but by itself is particularly useful
+        for health problems associated with stress, grief, and aging. Coloma
+        Resort To see natural beauty and connect with nature, visit this
+        gorgeous, adventurous outdoor place for a week or just a weekend. A
+        favorite getaway for the family... Perfect Balance Yoga Conveniently
+        located on the NW corner of Fresno and Shaw, Perfect Balance Yoga offers
+        all levels of yoga, from the basic to advanced. Perfect Balance Yoga
+        also offers kid’s yoga, pre and postnatal yoga, and hot yoga. PB yoga is
+        a Yoga Teacher Training School registered through Yoga Alliance, and
+        offers a 200 hour certification program.
       </div>
     </div>
   );
--- src/pages/testimonials.tsx
+++ src/pages/testimonials.tsx
@@ -4,7 +4,7 @@ const Testimonials = () => {
       <h3 className="my-4 text-4xl text-center font-semibold italic">
         Testimonials
       </h3>
-      <div className="px-20 text-justify pb-10 font-semibold">
+      <div className="px-20 text-justify pb-10">
         <p>
           Thanks so much for the wonderful sessions. Monika, you have taken me
           out of the deep dungeon of lethargy and depression which was really
@@ -13,50 +13,63 @@ const Testimonials = () => {
           anti-depressant medication. Then I thought of consulting you and that
           was the best decision I made. With your help my depression is gone, I
           feel better and have started to enjoy life once again and that too
-          without all the horrible side effects of modern medicine. Thanks for
-          turning my life around. K.S. Fresno, CA The changes we've noticed in
-          the yoga students who are practicing Ayurveda with you are astounding.
-          I asked one woman today, do you feel as good as you look? Her answer
-          was YES! We see that with your guidance and inspiration, you are
-          helping people to live life in a happy, healthy body. Thank you for
-          sharing your love and knowledge of Ayurvedic Medicine with us. Michael
-          and Melinda Downing, Directors of Yoga For Living, Fresno, CA I am a
-          patient of Monika Joshi for almost 4 months. Before the ayurvedic
-          treatment I had been taking a pain medication, prescribed by my
-          doctor. The medicine was to be taken only for a short period of time,
-          so I switched over to ayurvedic treatment. I am satisfied with Mrs.
-          Joshi's way of treating patients as she attends to them nicely. She is
-          very concerned for her patients and is always willing to help them,
-          whether it is regarding consultation for a medicine or regarding a
-          good and healthy diet. She was available whenever I wanted an
-          appointment with her. I am happy with the ayurvedic treatment as it
-          helps cure the illness with no side effects. S.G. Fresno, CA I’ve only
-          known Monika and the practice of Ayurveda for a few months now but the
-          teachings have been life changing. I am learning to let go of the
-          past, not dwell on the uncertainty of the future and most important to
-          live in the moment! The techniques of meditation that I have learned
-          have profoundly calmed and grounded me in dealing with my everyday
-          life as well as brought my yoga practice to a whole new level of
-          exhilaration and peacefulness. Through different food combinations and
-          herbal supplements I’ve learned how sacred preparing and eating meals
-          can be. My life has truly benefited. I consider her to be a wonderful
-          gift and I look forward to my journey in Ayurveda. Rebecca L
-          Pendleton, Fresno CA Following the recommendations of Monika Joshi has
-          been the best health decision I have made in years. Incorporating only
-          a few of her dietary suggestions has benefited me tremendously. I feel
-          calmer and more settled, my food cravings have disappeared and I am
-          losing weight without dieting. Ghee has been the best addition to my
-          diet. Ghee feels very nourishing, and lubricating, especially to my
-          dry throat. Ghee also satisfies my craving for fat and keeps me from
-          being hungry between meals and it is very easy to make at home. Monika
-          is the only practitioner that I know of who offers custom herb
-          formulas. She knew exactly what I needed and herbs have helped me a
-          lot. M.D. - Austin, TX
+          without all the horrible side effects of modern medicine.
+          <br /> Thanks for turning my life around.
+          <strong className="text-lime-600"> K.S. Fresno, CA.</strong>
         </p>
         <p className="my-4">
-          Monika is also a Registered Nurse. She currently practices Ayurveda in
-          Fresno, California. She is a sought out lecturer educating people on
-          various aspects of Ayurveda in the Central Valley.
+          The changes we've noticed in the yoga students who are practicing
+          Ayurveda with you are astounding. I asked one woman today, do you feel
+          as good as you look? Her answer was YES! We see that with your
+          guidance and inspiration, you are helping people to live life in a
+          happy, healthy body. Thank you for sharing your love and knowledge of
+          Ayurvedic Medicine with us.
+          <br />
+          <strong className="text-lime-600">
+            Michael and Melinda Downing, Directors of Yoga For Living, Fresno,
+            CA .
+          </strong>
+        </p>
+        <p className="my-4">
+          I am a patient of Monika Joshi for almost 4 months. Before the
+          ayurvedic treatment I had been taking a pain medication, prescribed by
+          my doctor. The medicine was to be taken only for a short period of
+          time, so I switched over to ayurvedic treatment. I am satisfied with
+          Mrs. Joshi's way of treating patients as she attends to them nicely.
+          She is very concerned for her patients and is always willing to help
+          them, whether it is regarding consultation for a medicine or regarding
+          a good and healthy diet. She was available whenever I wanted an
+          appointment with her. I am happy with the ayurvedic treatment as it
+          helps cure the illness with no side effects.
+          <strong className="text-lime-600"> S.G. Fresno, CA .</strong>
+        </p>
+        <p className="my-4">
+          I’ve only known Monika and the practice of Ayurveda for a few months
+          now but the teachings have been life changing. I am learning to let go
+          of the past, not dwell on the uncertainty of the future and most
+          important to live in the moment! The techniques of meditation that I
+          have learned have profoundly calmed and grounded me in dealing with my
+          everyday life as well as brought my yoga practice to a whole new level
+          of exhilaration and peacefulness. Through different food combinations
+          and herbal supplements I’ve learned how sacred preparing and eating
+          meals can be. My life has truly benefited. I consider her to be a
+          wonderful gift and I look forward to my journey in Ayurveda.{" "}
+          <strong className="text-lime-600">
+            Rebecca L Pendleton, Fresno CA.
+          </strong>
+        </p>
+        <p className="my-4">
+          Following the recommendations of Monika Joshi has been the best health
+          decision I have made in years. Incorporating only a few of her dietary
+          suggestions has benefited me tremendously. I feel calmer and more
+          settled, my food cravings have disappeared and I am losing weight
+          without dieting. Ghee has been the best addition to my diet. Ghee
+          feels very nourishing, and lubricating, especially to my dry throat.
+          Ghee also satisfies my craving for fat and keeps me from being hungry
+          between meals and it is very easy to make at home. Monika is the only
+          practitioner that I know of who offers custom herb formulas. She knew
+          exactly what I needed and herbs have helped me a lot.
+          <strong className="text-lime-600"> M.D. - Austin, TX.</strong>
         </p>
       </div>
     </div>
